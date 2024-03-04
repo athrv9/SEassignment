92cb4723683219bf65411f90f86734a83b0788fa
cyd12 <157189023+cyd12@users.noreply.github.com>
2024-03-04 14:27:32 +0530

    Update SmartCalculator.java

--- SmartCalculator.java
+++ SmartCalculator.java
@@ -88,7 +88,8 @@ public class SmartCalculator
 	        if (n == 0)
 	            return 1;
 	        else
-	            return n * calculateFactorial(n - 1);
+	            return n * calculateFactoria
+			    l(n - 1);
 	    }
 
 	    /**
